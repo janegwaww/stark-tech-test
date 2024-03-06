ea848c05694579b89363bc62ac3e7a6cfa26b001
unknown <tiglapiles@gmail.com>
2024-03-06 11:05:54 +0800

    feat: layout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-import * as React from 'react'
-import { Button } from '@mui/material'
+"use client";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Button variant="contained">Hello world</Button>
-      </div>
-    </main>
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">Menu</Button>
+          <Autocomplete
+            options={["The Godfather", "Pulp Fiction"]}
+            renderInput={(params) => <TextField {...params} label="Movie" />}
+          />
+          <Button color="inherit">
+            Theme
+            <DarkModeIcon />
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <div>{/* <Button variant="contained">Hello world</Button> */}hello</div>
+      </Container>
+    </Box>
   );
 }
